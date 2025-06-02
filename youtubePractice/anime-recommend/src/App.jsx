b60932551fd4e7fa61aa9c3a221e58c41b4ecfcc
sowhatnow_git <forkaliuse6500u@gmail.com>
2025-06-02 20:31:25 +0530

    hi

--- youtubePractice/anime-recommend/src/App.jsx
+++
@@ -1,6 +0,0 @@
-import { getTopAnime } from "./services/api";
-//import { useState,useEffect } from 'react'
-export default function App() {
-  const getAnime = getTopAnime();
-  console.log(getAnime);
-}
